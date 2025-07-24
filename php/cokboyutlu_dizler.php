379d3c8836e3188757c374bfd951ab361d5afdab
Altay Akyürek <120604586+Altay-Akyurek@users.noreply.github.com>
2025-07-25 01:58:35 +0300

    Add files via upload

---
+++ php/cokboyutlu_dizler.php
@@ -0,0 +1,15 @@
+<?php
+    $bolum = array(
+        "php_sinifi"=>array(
+            "Mehmet"."<br>"=>450,
+            "Altay"=> 550,
+        ),
+        "java_sinifi"=>array(
+            "ali"."<br>"=> 100,
+            "mahmut"=> 890,
+        ),
+
+
+    );
+    print_r($bolum["php_sinifi"]);
+?>
---
+++ php/do_while.php
@@ -0,0 +1,11 @@
+<?php
+  $sayi=0;
+    
+    do {
+        $sayi++;
+        echo $sayi."<br>";
+        
+    }
+    while($sayi==110)
+
+?>
